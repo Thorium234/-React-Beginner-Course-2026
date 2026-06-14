b95e0f2e7f6574a66f2103261a42b8fe672857fa
Thorium234 <inyangalapaul5@gmail.com>
2026-06-14 21:19:24 +0300

    Refactor App component to use Gallery instead of Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-function Profile() {
-  return (
-    <img
-      src="https://react.dev/images/docs/scientists/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
+import Gallery from './components/Gallery.js';
 
-export default function Gallery() {
+export default function App() {
   return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <Gallery />
   );
 }
