4c5397fd0ceb46a44819423b7c5c67c921e40ce8
Thorium234 <inyangalapaul5@gmail.com>
2026-06-15 21:01:42 +0300

    creating branching logic with JavaScript’s if and return statements.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,63 @@
-import Gallery from "./components/Gallery";
+// import Gallery from "./components/Gallery";
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <Gallery />
+//   );
+// }
+
+// function Item({ name, isPacked }) {
+//   return <li className="item">{name}</li>;
+// }
+
+// export default function PackingList() {
+//   return (
+//     <section>
+//       <h1>Sally Ride's Packing List</h1>
+//       <ul>
+//         <Item
+//           isPacked={true}
+//           name="Space suit"
+//         />
+//         <Item
+//           isPacked={true}
+//           name="Helmet with a golden leaf"
+//         />
+//         <Item
+//           isPacked={false}
+//           name="Photo of Tam"
+//         />
+//       </ul>
+//     </section>
+//   );
+// }
+
+function Item({ name, isPacked }) {
+  if (isPacked) {
+    return <li className="item">{name} ✅</li>;
+  }
+  return <li className="item">{name}</li>;
+}
+
+export default function PackingList() {
   return (
-    <Gallery />
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+          isPacked={true}
+          name="Space suit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
+      </ul>
+    </section>
   );
 }
+
